da5b6e8f2ef79d878d64170f7fc7548ffe62f4e1
eps0003 <eps0003@users.noreply.github.com>
2020-04-28 17:43:44 +1000

    Fixed issues with previous merge

--- DiceRollerInJava.java
+++ DiceRollerInJava.java
@@ -26,30 +26,24 @@ public class DiceRollerInJava {
         int sum = 0;
         System.out.println("Do you want to roll die ???");
         while (true) {
-            int result = dice.roll();
-
-            sum = sum + result;
-            if (high < result)
-            {
-                high = result;
-            }
-            num = num + 1;
-
-            System.out.println("dice face value:" + result);
-            dice.draw(result);
-            
-            System.out.println("Number of rolls:" + num);
-            System.out.println("Highest number achieved:" + high);
-            System.out.println("Sum of total outcomes:" + sum);
-            System.out.println("Roll again? (type no to quit):");
-
             String input = scanner.nextLine();
             if (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("Yes")) {
-                int result = dice.roll();
+				int result = dice.roll();
+
+				sum = sum + result;
+				if (high < result)
+				{
+					high = result;
+				}
+				num = num + 1;
+
                 System.out.println("dice face value:" + result);
                 dice.draw(result);
 
-                System.out.println("Roll again? (type no to quit):");
+				System.out.println("Number of rolls:" + num);
+				System.out.println("Highest number achieved:" + high);
+				System.out.println("Sum of total outcomes:" + sum);
+				System.out.println("Roll again? (type no to quit):");
 			} else if (input.equalsIgnoreCase("n") || input.equalsIgnoreCase("no")) {
                 System.out.println("Bye!");
                 scanner.close();
